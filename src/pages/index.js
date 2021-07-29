4c4d00d3bd66ad1297fba9c552ede916bed14f7f
ArildStark <87885663+ArildStark@users.noreply.github.com>
2021-07-29 09:29:59 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,22 +5,13 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-import NavHeader from "../components/header"
-import Divtextheader from "../components/divheadertext"
-
 const IndexPage = () => (
-  
-  <Divtextheader>
-</Divtextheader>
-  
-  
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-
-  <StaticImage
+    <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
       quality={95}
@@ -28,12 +19,9 @@ const IndexPage = () => (
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
     />
-  
-<p>
+    <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
   </Layout>
 )
-
-export default IndexPage
